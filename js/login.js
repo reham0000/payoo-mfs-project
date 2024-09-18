299914a3e4b63c42cf80d90e7d25672670d5eeec
reham0000 <amsarkerreham@gmail.com>
2024-09-18 21:34:01 +0600

    second commit

--- js/login.js
+++ js/login.js
@@ -1,6 +1,15 @@
+// document.getElementById('button-login').addEventListener('click',function(event){
+//     event.preventDefault();
+
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('pin-number').value;
+//     console.log(pinNumber, phoneNumber);
+// });
+
 document.getElementById('button-login').addEventListener('click',function(event){
     event.preventDefault();
 
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
-});
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber);
+})
