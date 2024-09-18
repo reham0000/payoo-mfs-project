d847fca3f51be149b0c67df0a8a3d264af46ac3f
reham0000 <amsarkerreham@gmail.com>
2024-09-18 23:24:35 +0600

    money adding system added

--- js/home.js
+++ js/home.js
@@ -3,4 +3,21 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
 
     const addMoneyInput = document.getElementById('input-add-money').value;
     const pinNumberInput = document.getElementById('input-pin-number').value;
+
+    if(pinNumberInput === '1234') {
+        console.log('Adding money to your account');
+
+        const balance = document.getElementById('account-balance').innerText;
+        
+        
+        
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else{
+        alert('Failed to add money! Please try again');
+    }
 });
