2fb7e2c62ea921c9b48209110bf84e757c725500
reham0000 <amsarkerreham@gmail.com>
2024-09-18 22:32:02 +0600

    added money event handler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,6 @@
-console.log('fasf');
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    event.preventDefault();
+
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+});
