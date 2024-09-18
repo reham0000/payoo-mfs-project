4af1fc12af72de31dea9ff3740c2fbef48b588ea
reham0000 <amsarkerreham@gmail.com>
2024-09-18 21:34:40 +0600

    edit some javascript code

--- js/login.js
+++ js/login.js
@@ -1,15 +1,8 @@
-// document.getElementById('button-login').addEventListener('click',function(event){
-//     event.preventDefault();
-
-//     const phoneNumber = document.getElementById('phone-number').value;
-//     const pinNumber = document.getElementById('pin-number').value;
-//     console.log(pinNumber, phoneNumber);
-// });
-
 document.getElementById('button-login').addEventListener('click',function(event){
     event.preventDefault();
 
     const phoneNumber = document.getElementById('phone-number').value;
     const pinNumber = document.getElementById('pin-number').value;
-    console.log(phoneNumber, pinNumber);
-})
+    console.log(pinNumber, phoneNumber);
+});
+
