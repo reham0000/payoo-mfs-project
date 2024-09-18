1b48695444c23ebffb8e27c512758c0bf0f72085
reham0000 <amsarkerreham@gmail.com>
2024-09-18 22:03:21 +0600

    make a new home file

--- js/login.js
+++ js/login.js
@@ -4,5 +4,13 @@ document.getElementById('button-login').addEventListener('click',function(event)
     const phoneNumber = document.getElementById('phone-number').value;
     const pinNumber = document.getElementById('pin-number').value;
     console.log(pinNumber, phoneNumber);
+
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('You are logged in');
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('Wrong phone number or pin');
+    }
 });
 
